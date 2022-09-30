6c736cf2687ea449fed79c836b33e1ad559266d1
danielf.s1555 <108942528+theguyoninternet@users.noreply.github.com>
2022-09-30 11:58:41 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,1 +1,18 @@
 
+let tabuada = 2;
+
+function escreva(){
+    //tabuada do dois
+    document.write(tabuada + " x 1 = "+(tabuada*1)+"</br>");
+    document.write(tabuada + " x 2 = "+(tabuada*2)+"<br>");
+    document.write(tabuada + " x 3 = "+(tabuada*3)+"<br>");
+    document.write(tabuada + " x 2 = "+(tabuada*4)+"<br>");
+    document.write(tabuada + " x 3 = "+(tabuada*5)+"<br>");
+    document.write(tabuada + " x 2 = "+(tabuada*6)+"<br>");
+    document.write(tabuada + " x 3 = "+(tabuada*7)+"<br>");
+    document.write(tabuada + " x 2 = "+(tabuada*8)+"<br>");
+    document.write(tabuada + " x 3 = "+(tabuada*9)+"<br>"); 
+    document.write(tabuada + " x 2 = "+(tabuada*10)+"<br>");
+
+    
+}
