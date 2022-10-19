a4b0841c67abbb273b69b3ea3ed1a72dfb09e35f
danielf.s1555 <108942528+theguyoninternet@users.noreply.github.com>
2022-10-18 21:34:55 -0300

    Add files via upload

--- estudo.js
+++ estudo.js
@@ -1,34 +1,15 @@
 
-let tabuada = 2;
-
-function escreva(){
-    //tabuada do dois
-    document.write(tabuada + " x 1 = "+(tabuada*1)+"</br>");
-    document.write(tabuada + " x 2 = "+(tabuada*2)+"<br>");
-    document.write(tabuada + " x 3 = "+(tabuada*3)+"<br>");
-    document.write(tabuada + " x 2 = "+(tabuada*4)+"<br>");
-    document.write(tabuada + " x 3 = "+(tabuada*5)+"<br>");
-    document.write(tabuada + " x 2 = "+(tabuada*6)+"<br>");
-    document.write(tabuada + " x 3 = "+(tabuada*7)+"<br>");
-    document.write(tabuada + " x 2 = "+(tabuada*8)+"<br>");
-    document.write(tabuada + " x 3 = "+(tabuada*9)+"<br>"); 
-    document.write(tabuada + " x 2 = "+(tabuada*10)+"<br>");
-
-    
-}
-function repete(){
- for(let i = 1; i <= 10; i++){
-   document.write("tabuada do " + i + "</br>"); 
-     for(let j=1; j <= 10; j++){
-      document.write(i + " X " + j + "=" + (j*i) + "</br>" );
-      }
-   document.write("</br>"); 
- }
- } 
-
-let lista = ["Banana","Uva","Abacate","Tomate"]; 
-function minhaLista(){
- document.write(lista[1]) 
+function total(){
+  let c = document.getElementById("valor").value;
+  let j = document.getElementById("juros").value;
+  let t = document.getElementById("meses").value;
+  let r = 0;
+  for(let i = 1; i <= t; i++){
+      r = c * (1 + (j/100));
+      document.write("Mês " + i + " valor: " + r +"<br>");
+      c = r;
+  }
+  document.write("Resultado: " + r);
 }
 
 
